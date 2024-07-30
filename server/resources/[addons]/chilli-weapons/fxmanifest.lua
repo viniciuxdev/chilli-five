24e967d297a2c56bd85e16e9396bd646c66d023c
viniciux <viniciuxntw@outlook.com>
2024-07-30 00:29:47 -0300

    weapons-archives

---
+++ server/resources/[addons]/chilli-weapons/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+files {
+	"data/**/*"
+}
+
+data_file "WEAPONCOMPONENTSINFO_FILE" "data/**/*.meta"
+data_file "PED_PERSONALITY_FILE" "data/**/*.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "data/**/*.meta"
+data_file "WEAPON_METADATA_FILE" "data/**/*.meta"
+data_file "WEAPONINFO_FILE" "data/**/*.meta"
+data_file "WEAPONINFO_FILE_PATCH" "data/**/*.meta"
